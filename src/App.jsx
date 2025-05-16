d7e1edd52097c01489bc7021ef101dbc808fd64c
Joane <138986247+japcunha@users.noreply.github.com>
2025-05-16 10:32:11 -0300

    alterações de layoute e dbugs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useRef, useState } from "react";
+import Home from "./Home.jsx";
+
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
---
+++ src/Home.jsx
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+import AddButton from "./Components/AddButton";
+import TrashButton from "./Components/TrashButton";
+import Product from "./Components/Product";
+
+export default function Home() {
+  const inputRef = useRef();
+  const [produtos, setprodutos] = useState([]);
+
+  const cliqueiNoBotao = () => {
+    const nome = inputRef.current.value.trim();
+    if (nome) {
+      const novoProduto = {
+        id: Date.now(),
+        nome,
+      };
+      setprodutos([...produtos, novoProduto]);
+      inputRef.current.value = "";
+    }
+  };
+
+  const pressEnter = (e) => {
+    if (e.key == "Enter") {
+      cliqueiNoBotao();
+    }
+  };
+  const deletarProduto = (id) => {
+    setprodutos(produtos.filter((p) => p.id !== id));
+  };
+
+  return (
+    <>
+      <div className="Container">
+        <h1>Lista de compras</h1>
+
+        <input
+          placeholder="produto..."
+          ref={inputRef}
+          onKeyDown={pressEnter}
+        ></input>
+        <AddButton className="AddButton" onClick={cliqueiNoBotao}>
+          Adicionar
+        </AddButton>
+
+        <p>lista:</p>
+        {produtos.map((produto) => (
+          <Product className="Product" key={produto.id}>
+            <p>{produto.nome}</p>
+            <TrashButton
+              className="TrashButton"
+              onClick={() => deletarProduto(produto.id)}
+            >
+              🗑️
+            </TrashButton>
+          </Product>
+        ))}
+      </div>
+    </>
+  );
+}
