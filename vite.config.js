e5496279daa22d4ebd43927d2fe1e39e33020d1a
Joane <138986247+japcunha@users.noreply.github.com>
2025-05-21 19:54:11 -0300

    debugging

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Lista-de-compras-React.git/',
+  base: '/Lista-de-compras-React/',
   plugins: [react()],
 
 });
